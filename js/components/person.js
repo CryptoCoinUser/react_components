9b8323fa4ef999cd5ac3ca3d88124d2a7869a966
CryptoCoinUser <adkantor@gmail.com>
2017-06-28 15:55:16 -0700

    fixed typo

--- js/components/person.js
+++ js/components/person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Person() {
     const name = 'CryptoCoinUser';
-    const imageUrl = 'https://avatars3.githubusercontent.com/u/6550474?v=3&s=460';
+    const imageUrl = 'https://avatars3.githubusercontent.com/u/6550474?v=3&s=460';  
     const job = 'user of crypto coins';
     return (
         <div className="person">
